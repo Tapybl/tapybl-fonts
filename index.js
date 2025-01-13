b9ab296ff79547c668ba74d0de60018c1038fc66
Juando <sieben.co@gmail.com>
2025-01-13 10:20:22 -0300

    A

---
+++ index.js
@@ -0,0 +1,12 @@
+{
+  "name": "tapybl_fonts",
+  "version": "1.1.0",
+  "description": "This is the module necessary to show icons in tapybl app.",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "Anitech Solutions",
+  "license": "ISC"
+}
